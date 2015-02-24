2a1922f3810706378b27414f28732676c99be038
Cameron Little <cameron@camlittle.com>
2015-02-23 20:56:06 -0800

    Initial Commit

---
+++ LinAlg/LinAlg.h
@@ -0,0 +1,19 @@
+//
+//  LinAlg.h
+//  LinAlg
+//
+//  Created by Cameron Little on 2/23/15.
+//  Copyright (c) 2015 Cameron Little. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LinAlg.
+FOUNDATION_EXPORT double LinAlgVersionNumber;
+
+//! Project version string for LinAlg.
+FOUNDATION_EXPORT const unsigned char LinAlgVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LinAlg/PublicHeader.h>
+
+
